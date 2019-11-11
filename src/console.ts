8ba2b808e295778beb17a5e01a5171428c79ca4f
johanblumenberg <johan.blumenberg@gmail.com>
2019-11-11 16:02:20 +0100

    Log complete log from failed processes using collect-log

--- src/console.ts
+++ src/console.ts
@@ -7,6 +7,8 @@ export interface IConsole {
 
 export interface ConsoleFactory {
     create(): IConsole;
+    active(c: IConsole): boolean;
+    discard(c: IConsole): void;
     done(c: IConsole): void;
 }
 
@@ -70,6 +72,14 @@ export class SerializedConsole implements ConsoleFactory {
         return c;
     }
 
+    active(c: IConsole) {
+        return c === this._active;
+    }
+
+    discard(c: IConsole) {
+        this._list = this._list.filter(_c => c !== _c);
+    }
+
     done(c: IConsole) {
         (c as SerializedConsoleImpl).finished.resolve();
     }
@@ -80,5 +90,10 @@ export class DefaultConsole implements ConsoleFactory {
         return console;
     }
 
+    active(c: IConsole) {
+        return true;
+    }
+
+    discard(c: IConsole) { }
     done(c: IConsole) { }
 }
